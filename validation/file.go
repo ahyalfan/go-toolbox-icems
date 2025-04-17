976f49ba4b2558a1ea0a4d09a522599fd131eb2f
ahyalfan <alfandi0857@gmail.com>
2025-04-17 12:49:38 +0700

    update validation file simple function valid extension

--- validation/file.go
+++ validation/file.go
@@ -2,17 +2,21 @@ package validation
 
 import (
 	"mime/multipart"
+	"slices"
 	"strings"
 )
 
 func IsValidFileExtension(filename string, validExtensions []string) bool {
 	ext := strings.ToLower(getFileExtension(filename))
-	for _, validExt := range validExtensions {
-		if ext == validExt {
-			return true
-		}
-	}
-	return false
+
+	// for _, validExt := range validExtensions {
+	// 	if ext == validExt {
+	// 		return true
+	// 	}
+	// }
+	// return false
+
+	return slices.Contains(validExtensions, ext)
 }
 
 func getFileExtension(filename string) string {
