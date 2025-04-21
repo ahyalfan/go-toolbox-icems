04da26d9ac2374a34a1e4f376a35d2ca78c81765
ahyalfan <alfandi0857@gmail.com>
2025-04-22 01:27:09 +0700

    add claims our package

--- auth/claims.go
+++ auth/claims.go
@@ -10,3 +10,12 @@ type UserClaimsSpesifikRole struct {
 	AppID    string `json:"app_id"`
 	jwt.RegisteredClaims
 }
+
+// MyClaims defines custom JWT claims including Username and Role,
+// embedding jwt.RegisteredClaims to support standard fields like exp, iat, etc.
+type UserClaims struct {
+	ID    string   `json:"id"`
+	Email string   `json:"email"`
+	Roles []string `json:"roles"`
+	jwt.RegisteredClaims
+}
