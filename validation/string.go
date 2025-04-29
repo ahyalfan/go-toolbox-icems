dda7497eebd20b7ddc3af472184705ab7afc0a63
ahyalfan <alfandi0857@gmail.com>
2025-04-29 14:48:50 +0700

    add String validation simple

---
+++ validation/string.go
@@ -0,0 +1,20 @@
+package validation
+
+// isAlphaNumericOrSpecial checks if a character is alphanumeric or a valid special character.
+func IsAlphaNumericOrSpecial(char rune) bool {
+	// Validate the character (alphanumeric, dash, underscore, or dot)
+	switch {
+	case 'A' <= char && char <= 'Z': // value char betwen runa 'A' and 'Z'
+		return true
+	case 'a' <= char && char <= 'z':
+		return true
+	case '0' <= char && char <= '9':
+		return true
+	case char == '-' || char == '_':
+		return true
+	case char == '.':
+		return true
+	default:
+		return false
+	}
+}
